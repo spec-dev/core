adb029e3de8e25539b3ca4aec5b725d5e06a2730
Ben Whittle <benwhittle31@gmail.com>
2022-08-30 02:02:13 -0500

    prep indexer yaml and scripts for k8s push

--- indexers/src/events/relay.ts
+++ indexers/src/events/relay.ts
@@ -3,12 +3,12 @@ import { StringKeyMap, logger } from '../../../shared'
 import { createEventClient } from '@spec.dev/event-client'
 import config from '../config'
 
-const eventClient = createEventClient({
-    hostname: config.EVENT_RELAY_HOSTNAME,
-    signedAuthToken: config.PUBLISHER_ROLE_KEY,
-})
+// const eventClient = createEventClient({
+//     hostname: config.EVENT_RELAY_HOSTNAME,
+//     signedAuthToken: config.PUBLISHER_ROLE_KEY,
+// })
 
 export async function emit(event: SpecEvent<StringKeyMap | StringKeyMap[]>) {
     logger.info(`Publishing ${event.name}...`)
-    eventClient.socket.transmitPublish(event.name, event)
+    // eventClient.socket.transmitPublish(event.name, event)
 }
