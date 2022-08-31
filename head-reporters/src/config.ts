7a53bc36ea76cc859e381f4c542753fe4bde4b78
Ben Whittle <benwhittle31@gmail.com>
2022-08-31 01:23:44 -0500

    head-reporter build script / Dockerfile touchups

--- head-reporters/src/config.ts
+++ head-reporters/src/config.ts
@@ -1,9 +1,11 @@
-import { ev, config, isNumber } from '../../shared'
+import { ev, config, isNumber, StringKeyMap } from '../../shared'
 
 const chainId = parseInt(ev('CHAIN_ID'))
 
-export default {
+const hrConfig: StringKeyMap = {
     ...config,
     CHAIN_ID: isNumber(chainId) ? chainId : null,
     ALCHEMY_SUBSCRIPTION_URL: `wss://eth-mainnet.g.alchemy.com/v2/${ev('ALCHEMY_API_KEY')}`,
 }
+
+export default hrConfig
