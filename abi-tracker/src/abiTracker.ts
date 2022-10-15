a6a905110ef870923edb74e57c5d18873fa4c67c
Ben Whittle <benwhittle31@gmail.com>
2022-10-15 18:45:17 -0500

    update user agent

--- abi-tracker/src/abiTracker.ts
+++ abi-tracker/src/abiTracker.ts
@@ -94,21 +94,9 @@ class AbiTracker {
         try {
             resp = await fetch(
                 `https://etherscan.io/contractsVerified/${pageNumber}`,
-                { 
+                {
                     signal: abortController.signal,
-                    headers: {
-                        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0",
-                        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-                        "Accept-Language": "en-US,en;q=0.5",
-                        "Accept-Encoding": "gzip, deflate",
-                        "Connection": "keep-alive",
-                        "Upgrade-Insecure-Requests": "1",
-                        "Sec-Fetch-Dest": "document",
-                        "Sec-Fetch-Mode": "navigate",
-                        "Sec-Fetch-Site": "none",
-                        "Sec-Fetch-User": "?1",
-                        "Cache-Control": "max-age=0",                
-                    }
+                    headers: {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
                 }
             )
         } catch (err) {
