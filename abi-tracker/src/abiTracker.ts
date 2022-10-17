2c24025cecf8a34b8c0bd2da222671b7e58fc772
Ben Whittle <benwhittle31@gmail.com>
2022-10-16 19:51:19 -0500

    update headers on abi-tracker

--- abi-tracker/src/abiTracker.ts
+++ abi-tracker/src/abiTracker.ts
@@ -96,7 +96,23 @@ class AbiTracker {
                 `https://etherscan.io/contractsVerified/${pageNumber}`,
                 {
                     signal: abortController.signal,
-                    headers: {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+                    headers: {
+                        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36',
+                        'cookie': 'ASP.NET_SessionId=loa53vy15uci55vdew45i4cn; __cflb=02DiuFnsSsHWYH8WqVXbZzkeTrZ6gtmGUnUMV4ZqgKaCc; _ga=GA1.2.2116816403.1665967300; _gid=GA1.2.1601611006.1665967300; __cf_bm=NJ3ZUwA7wyHiVGzBgBDXdIV0UDe4TWmRgZUwqm9Bw4U-1665967300-0-ASlcAi2Mt/QK1qoNfTJFilV3v35nS7XQ2lcMa66EME9gFPAaYt6jiummtJMbEui5WNuYUYLCqKWoPMrj9DRePqylzbHr+Ve9JKVLmaiu6XSPtxc1bTtcAEzgOmqbqXZZAw==',
+                        'accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+                        'accept-language': "en-US,en;q=0.9",
+                        'accept-encoding': "gzip, deflate, br",
+                        'upgrade-insecure-requests': "1",
+                        "sec-fetch-dest": "document",
+                        "sec-fetch-mode": "navigate",
+                        "sec-fetch-site": "none",
+                        "sec-fetch-user": "?1",
+                        'sec-ch-ua': '"Chromium";v="106", "Google Chrome";v="106", "Not;A=Brand";v="99"',
+                        'sec-ch-ua-mobile': '?0',
+                        'sec-ch-ua-platform': '"macOS"',
+                        'cache-control': 'no-cache',
+                        'pragma': 'no-cache'
+                    }
                 }
             )
         } catch (err) {
