b94d2acb6d427e148a9bdb251d65b038f71a7a94
Ben Whittle <benwhittle31@gmail.com>
2022-10-29 00:48:43 -0500

    testing hacks

--- indexers/src/index.ts
+++ indexers/src/index.ts
@@ -6,8 +6,14 @@ import {
     SharedTables,
     CoreDB,
     upsertContractCaches,
+    getAbi,
+    abiRedis,
 } from '../../shared'
 import { getWorker } from './workers'
+
+import Web3 from 'web3'
+const web3 = new Web3()
+const anotherWeb3 = new Web3('https://eth-mainnet.g.alchemy.com/v2/-ZWlJMwtKBGm_L04mia-tfGvfBipwtXs')
 
 async function run() {
     // Start all databases.
@@ -16,19 +22,42 @@ async function run() {
         SharedTables.initialize(),
         CoreDB.initialize(),
         indexerRedis.connect(),
+        abiRedis.connect(),
     ])
 
-    // Make sure verified contracts and instances are cached.
-    await upsertContractCaches()
+    // // Make sure verified contracts and instances are cached.
+    // await upsertContractCaches()
 
-    logger.info(
-        config.IS_RANGE_MODE
-            ? `Indexing block range ${config.FROM} -> ${config.TO}...`
-            : `Listening for new block heads...`
-    )
+    // logger.info(
+    //     config.IS_RANGE_MODE
+    //         ? `Indexing block range ${config.FROM} -> ${config.TO}...`
+    //         : `Listening for new block heads...`
+    // )
 
-    const worker = await getWorker()
-    worker.run()
+    // const worker = await getWorker()
+    // worker.run()
+
+
+    const addr = '0x862309bd8802061293541645a1e09e677cfe571a'
+    const abi = JSON.parse((await getAbi(addr)) as any)
+
+    for (const item of abi) {
+        switch (item.type) {
+            case 'function':
+                console.log(web3.eth.abi.encodeFunctionSignature(item as any))
+            case 'event':
+                console.log(web3.eth.abi.encodeEventSignature(item as any))
+        }
+    }
+
+    for (const item of abi) {
+        switch (item.type) {
+            case 'function':
+                console.log(anotherWeb3.eth.abi.encodeFunctionSignature(item as any))
+            case 'event':
+                console.log(anotherWeb3.eth.abi.encodeEventSignature(item as any))
+        }
+    }
 }
 
 run()
