b2ddd45ef713f6a0f54d4a30219948f46841fbf7
Ben Whittle <benwhittle31@gmail.com>
2022-11-02 10:02:48 -0500

    add mumbai chain id as const

--- shared/src/lib/utils/chainIds.ts
+++ shared/src/lib/utils/chainIds.ts
@@ -1,6 +1,7 @@
 const chainIds: { [key: string]: number } = {
     ETH_MAINNET: 1,
     POLYGON_MAINNET: 137,
+    POLYGON_MUMBAI: 80001,
 }
 
 export const productionChainNameForChainId = (chainId: number): string | null => {
@@ -8,6 +9,7 @@ export const productionChainNameForChainId = (chainId: number): string | null =>
         case chainIds.ETH_MAINNET:
             return 'eth'
         case chainIds.POLYGON_MAINNET:
+        case chainIds.POLYGON_MUMBAI:
             return 'polygon'
         default:
             return null
