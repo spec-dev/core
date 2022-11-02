3ca5bbdccc04ee79ea4e061acead38c343219b38
Ben Whittle <benwhittle31@gmail.com>
2022-11-02 16:39:40 -0500

    update abi polisher to add sigs

--- indexers/src/workers/abiPolisher.ts
+++ indexers/src/workers/abiPolisher.ts
@@ -17,12 +17,8 @@ import Web3 from 'web3'
 
 const web3 = new Web3()
 
-const ivyAbi = [{"type":"constructor","payable":false,"inputs":[{"type":"address","name":"trustedForwarder_"}],"signature":"0x88495b5f"},{"type":"event","anonymous":false,"name":"OwnershipTransferred","inputs":[{"type":"address","name":"previousOwner","indexed":true},{"type":"address","name":"newOwner","indexed":true}],"signature":"0x8be0079c531659141344cd1fd0a4f28419497f9722a3daafe3b4186f6b6457e0"},{"type":"event","anonymous":false,"name":"SmartWalletImpSet","inputs":[{"type":"address","name":"smartWalletImpl","indexed":true}],"signature":"0xccf229c32b4b7ac08f8e696ce64fa21c8f59b7c7662fa66ceebe5bdf2a144f67"},{"type":"event","anonymous":false,"name":"WalletCreated","inputs":[{"type":"address","name":"owner","indexed":true},{"type":"address","name":"smartWallet","indexed":true}],"signature":"0x5b03bfed1c14a02bdeceb5fa582eb1a5765fc0bc64ca0e6af4c20afc9487f081"},{"type":"function","name":"getSmartWalletImpl","constant":true,"stateMutability":"view","payable":false,"inputs":[],"outputs":[{"type":"address"}],"signature":"0x6e067962"},{"type":"function","name":"initWallet","constant":false,"payable":false,"inputs":[{"type":"address","name":"user_"}],"outputs":[{"type":"address"}],"signature":"0x9da8be21"},{"type":"function","name":"initWalletWithPayment","constant":false,"stateMutability":"payable","payable":true,"inputs":[{"type":"address","name":"user_"}],"outputs":[{"type":"address"}],"signature":"0x5660356c"},{"type":"function","name":"isTrustedForwarder","constant":true,"stateMutability":"view","payable":false,"inputs":[{"type":"address","name":"forwarder"}],"outputs":[{"type":"bool"}],"signature":"0x572b6c05"},{"type":"function","name":"name","constant":true,"stateMutability":"view","payable":false,"inputs":[],"outputs":[{"type":"string"}],"signature":"0x06fdde03"},{"type":"function","name":"owner","constant":true,"stateMutability":"view","payable":false,"inputs":[],"outputs":[{"type":"address"}],"signature":"0x8da5cb5b"},{"type":"function","name":"renounceOwnership","constant":false,"payable":false,"inputs":[],"outputs":[],"signature":"0x715018a6"},{"type":"function","name":"setSmartWalletImplementation","constant":false,"payable":false,"inputs":[{"type":"address","name":"smartWalletImpl_"}],"outputs":[],"signature":"0x3b08da7c"},{"type":"function","name":"transferOwnership","constant":false,"payable":false,"inputs":[{"type":"address","name":"newOwner"}],"outputs":[],"signature":"0xf2fde38b"}]
-const ivyAbiMap = {
-    '0x395b1b4cbd34c1ec7aaf47e6bf2a2356af558fe2': JSON.stringify(ivyAbi)
-}
-
 class AbiPolisher {
+
     from: number 
 
     to: number | null
@@ -39,40 +35,28 @@ class AbiPolisher {
     }
 
     async run() {
-        await saveAbis(ivyAbiMap, abiRedisKeys.POLYGON_CONTRACTS)
+        await abiRedis.del(abiRedisKeys.ETH_FUNCTION_SIGNATURES)
 
+        let cursor = null
+        let batch
+        while (true) {
+            const results = await this._getAbisBatch(cursor || 0)
+            cursor = results[0]
+            batch = results[1]
+            logger.info(`CURSOR: ${cursor}`)
+            const [abisMapToSave, funcSigHashesMap] = await this._polishAbis(batch)
+
+            await Promise.all([
+                this._saveAbis(abisMapToSave), 
+                this._saveFuncSigHashes(funcSigHashesMap),
+            ])
+
+            if (cursor === 0) {
+                break
+            }
+        }
         logger.info('DONE')
         exit()
-
-        // let cursor = null
-        // let batch
-        // while (true) {
-        //     const results = await this._getAbisBatch(cursor || 0)
-        //     cursor = results[0]
-        //     batch = results[1]
-
-        //     logger.info(`CURSOR: ${cursor}`)
-
-        //     await this._polishAbis(batch)
-    
-        //     // await Promise.all([
-        //     //     this._saveAbis(abisMapToSave), 
-        //     //     this._saveFuncSigHashes(funcSigHashesMap),
-        //     // ])
-
-        //     if (cursor === 0) {
-        //         break
-        //     }
-    
-        //     // const start = this.cursor
-        //     // const end = Math.min(this.cursor + this.groupSize - 1, this.to)
-        //     // const group = range(start, end)
-        //     // await this._indexGroup(group)
-        //     // this.cursor = this.cursor + this.groupSize
-        // }
-        // logger.info('DONE')
-        // logger.info(await abiRedis.sCard('refetch-abis2'))
-        // exit()
     }
 
     async _indexGroup(numbers: number[]) {
@@ -84,7 +68,7 @@ class AbiPolisher {
 
         // logger.info(`    Got ${addressAbis.length} ABIs to polish starting at ${addressAbis[0]?.address}.`)
 
-        // const [abisMapToSave, funcSigHashesMap] = this._polishAbis(addressAbis)
+        // const [abisMapToSave, funcSigHashesMap] = await this._polishAbis(addressAbis)
 
         // await Promise.all([
         //     this._saveAbis(abisMapToSave), 
@@ -93,9 +77,6 @@ class AbiPolisher {
     }
 
     async _getAbisBatch(inputCursor: number) {
-        // const offset = numbers[0]
-        // const limit = numbers.length
-
         let results
         try {
             results = await abiRedis.hScan('eth-contracts', inputCursor, { COUNT: 1000, MATCH: '*' })
@@ -126,8 +107,6 @@ class AbiPolisher {
         const abisToUpdate = {}
         const funcSigHashesMap = {}
 
-        const refetchSet = new Set()
-
         for (const entry of addressAbis) {
             const { address, abi = [] } = entry
 
@@ -135,10 +114,9 @@ class AbiPolisher {
             let modified = false
             for (const item of abi) {
                 if (item.inputs?.includes(null)) {
-                    refetchSet.add(address)
-                    break
-                    // newAbi.push(item)
-                    // continue
+                    logger.info('STILL GOT NULL!', address)
+                    newAbi.push(item)
+                    continue
                 }
 
                 let signature = item.signature
@@ -155,26 +133,21 @@ class AbiPolisher {
                     }
                 }
 
-                // if (['function', 'constructor'].includes(item.type) && signature && !funcSigHashesMap.hasOwnProperty(signature)) {
-                //     funcSigHashesMap[signature] = {
-                //         name: item.name,
-                //         type: item.type,
-                //         inputs: (item.inputs || []).map(({ type }) => ({ type })),
-                //         signature,
-                //     }
-                // }
+                if (['function', 'constructor'].includes(item.type) && signature && !funcSigHashesMap.hasOwnProperty(signature)) {
+                    funcSigHashesMap[signature] = {
+                        name: item.name,
+                        type: item.type,
+                        inputs: (item.inputs || []).map(({ type }) => ({ type })),
+                        signature,
+                    }
+                }
             }
             if (modified) {
-                // abisToUpdate[address] = newAbi
+                abisToUpdate[address] = newAbi
             }
         }
 
-        const refetch = Array.from(refetchSet) as string[]
-        if (refetch.length) {
-            await abiRedis.sAdd('refetch-abis2', refetch)
-        }
-
-        // return [abisToUpdate, funcSigHashesMap]
+        return [abisToUpdate, funcSigHashesMap]
     }
 
     _createAbiItemSignature(item: StringKeyMap): string | null {
