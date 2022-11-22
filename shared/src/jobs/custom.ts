470adbb4d664499ca33beaf48f40d309d10ade12
Ben Whittle <benwhittle31@gmail.com>
2022-11-22 00:00:05 -0500

    custom data job

--- shared/src/jobs/custom.ts
+++ shared/src/jobs/custom.ts
@@ -1,29 +1,9 @@
-import { abiRedisKeys, getAbi, redis, saveAbis } from '../lib/abi/redis'
+import { redis } from '../lib/indexer/redis'
 import { exit } from 'process'
 
 async function perform() {
     await redis.connect()
-    const abi = await getAbi(
-        '0x395b1b4cbd34c1ec7aaf47e6bf2a2356af558fe2',
-        abiRedisKeys.POLYGON_CONTRACTS
-    )
-    if (!abi) {
-        console.log('No abi found')
-        return
-    }
-
-    const abiStr = JSON.stringify(abi)
-
-    console.log('Saving abis...')
-
-    await saveAbis(
-        {
-            '0xfce05688b59bd8491f99cc5dcd34ce1b2175741f': abiStr,
-            '0xb0cd4333a9aa432144b678f8ec87ed88a44e151f': abiStr,
-        },
-        abiRedisKeys.POLYGON_CONTRACTS
-    )
-
+    await redis.del('polygon-head-reporter')
     exit(0)
 }
 
