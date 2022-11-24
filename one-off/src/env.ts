dbefb7b637e858c26b1a615a6994cef9e04b8a30
Ben Whittle <benwhittle31@gmail.com>
2022-11-24 14:26:39 -0500

    get rid of one-off; updated deploy scripts

--- one-off/src/env.ts
+++
@@ -1,8 +0,0 @@
-export const ev = (name, fallback = null) =>
-    process.env.hasOwnProperty(name) ? process.env[name] : fallback
-
-export const specEnvs = {
-    LOCAL: 'local',
-    STAGING: 'staging',
-    PROD: 'prod',
-}
