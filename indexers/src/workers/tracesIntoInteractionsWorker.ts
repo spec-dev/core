c51202b0583358a4d33b66fa4124ab981f252414
Ben Whittle <benwhittle31@gmail.com>
2022-11-28 23:33:16 -0600

    tweak traces to interactions on conflict clause

--- indexers/src/workers/tracesIntoInteractionsWorker.ts
+++ indexers/src/workers/tracesIntoInteractionsWorker.ts
@@ -24,7 +24,7 @@ class TracesToInteractionsWorker {
     groupSize: number
     
     seenContracts: LRU<string, boolean> = new LRU({
-        max: 5000,
+        max: 10000,
     })
 
     constructor(from: number, groupSize?: number) {
@@ -95,14 +95,20 @@ class TracesToInteractionsWorker {
             interaction.interactionType = [fromType, toType].join(':') as EthLatestInteractionType
         })
 
-        await SharedTables.manager.transaction(async (tx) => {
-            await tx
-                .createQueryBuilder()
-                .insert()
-                .into(EthLatestInteraction)
-                .values(latestInteractions)
-                .onConflict(`("from", "to") DO NOTHING`)
-                .execute()
+        const insertPlaceholders = []
+        const insertBindings = []
+        let i = 1
+        for (const li of latestInteractions) {
+            insertPlaceholders.push(`($${i}, $${i + 1}, $${i + 2}, $${i + 3}, $${i + 4}, $${i + 5}, $${i + 6})`)
+            insertBindings.push(...[li.from, li.to, li.interactionType, li.hash, li.timestamp, li.blockHash, li.blockNumber])
+            i += 7
+        }
+
+        await SharedTables.manager.transaction(async tx => {
+            await tx.query(
+                `INSERT INTO ethereum.latest_interactions ("from", "to", interaction_type, hash, timestamp, block_hash, block_number) VALUES ${insertPlaceholders.join(', ')} ON CONFLICT ("from", "to") DO UPDATE SET timestamp = EXCLUDED.timestamp, interaction_type = EXCLUDED.interaction_type, hash = EXCLUDED.hash, block_hash = EXCLUDED.block_hash, block_number = EXCLUDED.block_number WHERE ethereum.latest_interactions.block_number < EXCLUDED.block_number`,
+                insertBindings,    
+            )
         })
     }
 
