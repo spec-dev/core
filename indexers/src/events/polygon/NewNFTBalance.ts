ea60be4184fe62dfca5cf94dab03d7d6d06e1203
Ben Whittle <benwhittle31@gmail.com>
2022-12-01 02:55:40 -0600

    fix table name

--- indexers/src/events/polygon/NewNFTBalance.ts
+++ indexers/src/events/polygon/NewNFTBalance.ts
@@ -11,7 +11,7 @@ export async function onNewNFTBalanceEvent(eventSpec: StringKeyMap): Promise<Str
     } = origin
     try {
         if (!data.balance || Number(data.balance) === 0) {
-            await SharedTables.query(`DELETE FROM polygon.nft_balances WHERE token_address = $1 AND token_id = $2 AND owner_address = $3 AND chain_id = $4`, 
+            await SharedTables.query(`DELETE FROM tokens.nft_balances WHERE token_address = $1 AND token_id = $2 AND owner_address = $3 AND chain_id = $4`, 
                 [
                     data.tokenAddress,
                     data.tokenId,
@@ -21,7 +21,7 @@ export async function onNewNFTBalanceEvent(eventSpec: StringKeyMap): Promise<Str
             )
         } else {
             await SharedTables.query(
-                `INSERT INTO polygon.nft_balances (token_address, token_name, token_symbol, token_standard, token_id, owner_address, balance, block_number, block_hash, block_timestamp, chain_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) ON CONFLICT (token_address, token_id, owner_address, chain_id) DO UPDATE SET balance = EXCLUDED.balance, block_number = EXCLUDED.block_number, block_hash = EXCLUDED.block_hash, block_timestamp = EXCLUDED.block_timestamp`,
+                `INSERT INTO tokens.nft_balances (token_address, token_name, token_symbol, token_standard, token_id, owner_address, balance, block_number, block_hash, block_timestamp, chain_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) ON CONFLICT (token_address, token_id, owner_address, chain_id) DO UPDATE SET balance = EXCLUDED.balance, block_number = EXCLUDED.block_number, block_hash = EXCLUDED.block_hash, block_timestamp = EXCLUDED.block_timestamp`,
                 [
                     data.tokenAddress,
                     data.tokenName,
