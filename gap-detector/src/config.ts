f430ef9a136bd10fbeb8cf045963616cab09e6f7
Ben Whittle <benwhittle31@gmail.com>
2022-12-09 22:47:04 -0600

    up gap tolerance to 5

--- gap-detector/src/config.ts
+++ gap-detector/src/config.ts
@@ -3,7 +3,7 @@ import { ev, config, StringKeyMap } from '../../shared'
 const gapDetectorConfig: StringKeyMap = {
     ...config,
     NEW_BLOCK_CHANNEL_PREFIX: 'new_block_chain_',
-    GAP_TOLERANCE: 3,
+    GAP_TOLERANCE: Number(ev('GAP_TOLERANCE', 5)),
     ETHEREUM_HEAD_REPORTER_QUEUE_KEY: ev('ETHEREUM_HEAD_REPORTER_QUEUE_KEY'),
     POLYGON_HEAD_REPORTER_QUEUE_KEY: ev('POLYGON_HEAD_REPORTER_QUEUE_KEY'),
     MUMBAI_HEAD_REPORTER_QUEUE_KEY: ev('MUMBAI_HEAD_REPORTER_QUEUE_KEY'),
