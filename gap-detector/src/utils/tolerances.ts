c62479af16ee45f4fe456070515866fa1dc42209
Ben Whittle <benwhittle31@gmail.com>
2022-12-11 22:03:22 -0600

    bump gap tolerance down from 7 -> 5 for poly

--- gap-detector/src/utils/tolerances.ts
+++ gap-detector/src/utils/tolerances.ts
@@ -2,6 +2,6 @@ import { chainIds } from '../../../shared'
 
 export const gapTolerances = {
     [chainIds.ETHEREUM]: 4,
-    [chainIds.POLYGON]: 7,
+    [chainIds.POLYGON]: 5,
     [chainIds.MUMBAI]: 5,
 }
