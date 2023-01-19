9d0a1e07bb8565aae754de68f568e1a21149f961
Ben Whittle <benwhittle31@gmail.com>
2023-01-18 18:37:41 -0600

    fix duplicate properties in NewContract evnet

--- indexers/src/events/eth/NewContracts/index.ts
+++ indexers/src/events/eth/NewContracts/index.ts
@@ -4,10 +4,12 @@ const eventName = 'eth.NewContracts@0.0.1'
 
 function NewContracts(contracts: EthContract[], eventOrigin: StringKeyMap): StringKeyMap {
     const eventData = contracts.map((contract) => ({
-        ...contract,
+        address: contract.address,
+        bytecode: contract.bytecode,
         isErc20: contract.isERC20,
         isErc721: contract.isERC721,
         isErc1155: contract.isERC1155,
+        blockHash: contract.blockHash,
         blockNumber: Number(contract.blockNumber),
         blockTimestamp: contract.blockTimestamp.toISOString(),
     }))
