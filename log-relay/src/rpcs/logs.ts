26f328af870e2b604f1bc101ca6efcef9854155f
Ben Whittle <benwhittle31@gmail.com>
2023-02-16 20:01:24 -0600

    add consolidated bugsnag project for logging spec client errors

--- log-relay/src/rpcs/logs.ts
+++ log-relay/src/rpcs/logs.ts
@@ -1,8 +1,14 @@
-import { Log } from '../types'
-import { addLog } from '../../../shared'
+import { Log, LogLevel } from '../types'
+import { addLog, logger } from '../../../shared'
 
 export function processLog(log: Log) {
     const { message = '', level, timestamp, projectId } = log
     if (!level || !timestamp) return
+
+    // Add log to project's redis stream.
     addLog(projectId, { message, level, timestamp })
+
+    if (level === LogLevel.Error) {
+        logger.error(`[${projectId}] Spec Client Error - ${message}`)
+    }
 }
