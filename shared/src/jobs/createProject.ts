a684eff09f6652881ae7b4f5fa097e71804db115
Ben Whittle <benwhittle31@gmail.com>
2023-02-20 22:10:58 -0600

    debugging

--- shared/src/jobs/createProject.ts
+++ shared/src/jobs/createProject.ts
@@ -2,17 +2,39 @@ import logger from '../lib/logger'
 import { CoreDB } from '../lib/core/db/dataSource'
 import { exit } from 'process'
 import { createProject } from '../lib/core/db/services/projectServices'
+import { indexerRedis } from '..'
+import { Queue, QueueEvents } from 'bullmq'
+import config from '../lib/config'
 
-async function perform(name: string, orgId: number) {
-    await CoreDB.initialize()
+async function perform() {
+    await indexerRedis.connect()
+    await indexerRedis.del('block-events-series-137')
+    await indexerRedis.del('block-events-skipped-blocks-137')
+    await indexerRedis.del('block-events-eager-blocks-137')
 
-    logger.info(`Creating project ${name}...`)
-    const project = await createProject(name, Number(orgId))
+    const queue = new Queue('block-events-queue-137', {
+        connection: {
+            host: config.INDEXER_REDIS_HOST,
+            port: config.INDEXER_REDIS_PORT,
+        },
+    })
+    await queue.obliterate()
 
-    if (!project) {
-        logger.info('Failed.')
-        exit(0)
-    }
+    const queue2 = new Queue('event-gen-queue-137', {
+        connection: {
+            host: config.INDEXER_REDIS_HOST,
+            port: config.INDEXER_REDIS_PORT,
+        },
+    })
+    await queue2.obliterate()
+
+    // logger.info(`Creating project ${name}...`)
+    // const project = await createProject(name, Number(orgId))
+
+    // if (!project) {
+    //     logger.info('Failed.')
+    //     exit(0)
+    // }
 
     logger.info(`Success.`)
     exit(0)
