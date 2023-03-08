507e01bdbcfccfd28bf6827061533caa5b6a495f
Ben Whittle <benwhittle31@gmail.com>
2023-03-07 19:40:31 -0600

    custom job

--- shared/src/jobs/custom.ts
+++ shared/src/jobs/custom.ts
@@ -1,12 +1,24 @@
-import { redis } from '../lib/indexer/redis'
 import { exit } from 'process'
 import { Queue } from 'bullmq'
+import config from '../lib/config'
 
 async function perform() {
-    await redis.connect()
-
-    await redis.del('polygon-contract-cache')
-
+    const queueEth = new Queue([config.BLOCK_EVENTS_QUEUE_PREFIX, '1'].join('-'), {
+        connection: {
+            host: config.INDEXER_REDIS_HOST,
+            port: config.INDEXER_REDIS_PORT,
+        },
+    })
+    await queueEth.obliterate()
+    console.log('Success')
+    const queueMumbai = new Queue([config.BLOCK_EVENTS_QUEUE_PREFIX, '80001'].join('-'), {
+        connection: {
+            host: config.INDEXER_REDIS_HOST,
+            port: config.INDEXER_REDIS_PORT,
+        },
+    })
+    await queueMumbai.obliterate()
+    console.log('Success')
     exit(0)
 }
 
