b556488ccf55ea0ee86d3bb9341b5298bb20d0b7
Ben Whittle <benwhittle31@gmail.com>
2023-03-10 23:12:49 -0600

    tweak decodetrace batch size

--- indexers/src/workers/decodeTraceWorker.ts
+++ indexers/src/workers/decodeTraceWorker.ts
@@ -105,7 +105,7 @@ export class DecodeTraceWorker {
 
         this.tracesToSave.push(...traces)
 
-        if (this.tracesToSave.length >= 3000) {
+        if (this.tracesToSave.length >= 5000) {
             await this._updateTraces(this.tracesToSave)
             this.tracesToSave = []
         }
