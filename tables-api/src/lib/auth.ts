c5703a7db795699807b32551a17f9ce3dcc9bd72
Ben Whittle <benwhittle31@gmail.com>
2023-03-23 00:49:07 -0500

    upgrade tables-api to accept object filters directly

--- tables-api/src/lib/auth.ts
+++ tables-api/src/lib/auth.ts
@@ -1,10 +1,10 @@
 import { verifyJWT } from '../../../shared'
 import config from './config'
+import { StringKeyMap } from './types'
 
-export function authRequest(req: any): string | null {
+export function authRequest(req: any): StringKeyMap {
     const headers = req.headers || {}
     const jwt = headers[config.AUTH_HEADER_NAME] || headers[config.AUTH_HEADER_NAME.toLowerCase()]
     if (!jwt) return null
-    const claims = verifyJWT(jwt)
-    return claims?.role || null
+    return verifyJWT(jwt) || {}
 }
--- tables-api/src/lib/payload.ts
+++ tables-api/src/lib/payload.ts
@@ -1,19 +1,75 @@
-import { QueryPayload, StringKeyMap } from './types'
+import { StringKeyMap } from './types'
 import { logger } from '../../../shared'
+import config from './config'
+import { buildSelectQuery, buildUpsertQuery, QueryPayload } from '@spec.dev/qb'
 
 const MAX_TX_ENTRIES = 10
 
-export function getQueryPayload(body: StringKeyMap): [QueryPayload, boolean] {
-    const payload = body as QueryPayload
-    return [payload, !!payload.sql]
+export function getQueryPayload(payload: StringKeyMap, claims: StringKeyMap): [StringKeyMap, boolean] {
+    if (!payload || typeof payload !== 'object' || Array.isArray(payload)) {
+        return [payload, false]
+    }
+
+    // Check if allowed to pass raw 'sql' param.
+    const hasSql = payload.hasOwnProperty('sql')
+    const canUseSql = (claims.id && config.V0_PAYLOAD_WHITELIST.includes(claims.id)) || claims.role === 'lens'
+    if (hasSql) {
+        const sql = (payload.sql || '').toLowerCase()
+        if (!sql.startsWith('select ') && !sql.startsWith('insert ')) {
+            return [payload, false]
+        }
+        return [payload, canUseSql]
+    }
+
+    try {
+        const query = buildQueryFromPayload(payload)
+        return query ? [query, true] : [payload, false]
+    } catch (err) {
+        return [payload, false]
+    } 
 }
 
-export function getTxPayload(body: StringKeyMap[]): [QueryPayload[], boolean] {
-    const payload = (body || []) as QueryPayload[]
-    if (!payload.length) return [[], false]
+export function getTxPayload(payload: StringKeyMap[], claims: StringKeyMap): [StringKeyMap[], boolean] {
+    if (!payload) return [payload, false]
+    payload = payload || []
+    payload = Array.isArray(payload) ? payload : [payload]
+    if (!payload.length) {
+        return [payload, false]
+    }
     if (payload.length > MAX_TX_ENTRIES) {
         logger.info(`Tx got more than max allowed entries`, payload.length)
-        return [[], false]
+        return [payload, false]
     }
-    return [payload, payload.every(p => p?.sql && !!(p?.bindings?.length))]
+
+    const queries = []
+    for (const entry of payload) {
+        const [query, isValid] = getQueryPayload(entry, claims)
+        if (!isValid) return [payload, false]
+        queries.push(query)
+    }
+    return [queries, true]
+}
+
+function buildQueryFromPayload(payload: StringKeyMap): QueryPayload | null {
+    payload = payload || {}
+    const table = payload.table
+    if (!table) return null
+
+    // SELECT
+    if (payload.hasOwnProperty('filters')) {
+        return buildSelectQuery(table, payload.filters, payload.options)
+    }
+
+    // UPSERT
+    if (payload.data && payload.hasOwnProperty('conflictColumns') && payload.hasOwnProperty('updateColumns')) {
+        return buildUpsertQuery(
+            table,
+            payload.data,
+            payload.conflictColumns || [],
+            payload.updateColumns || [],
+            payload.returning,
+        )
+    }
+
+    return null
 }
