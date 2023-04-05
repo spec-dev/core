8e029c350f8dbcbd1b19eaeb300647809aa8fdd0
Ben Whittle <benwhittle31@gmail.com>
2023-04-05 12:24:24 -0500

    new custom job

--- shared/src/jobs/custom.ts
+++ shared/src/jobs/custom.ts
@@ -1,10 +1,24 @@
 import { redis } from '../lib/core/redis'
 import { exit } from 'process'
+import { Queue, QueueScheduler } from 'bullmq'
+import config from '../lib/config'
 
 async function perform() {
-    await redis.connect()
-    await redis.del('lajaijveutrfanbufusu')
-    await redis.del('lajaijveutrfanbufusu-staging')
+    const queue = new Queue(config.DELAYED_JOB_QUEUE_KEY, {
+        connection: {
+            host: config.INDEXER_REDIS_HOST,
+            port: config.INDEXER_REDIS_PORT,
+        },
+        defaultJobOptions: {
+            attempts: 2,
+            backoff: {
+                type: 'fixed',
+                delay: 1000,
+            },
+        },
+    })
+
+    await queue.drain()
     exit(0)
 }
 
