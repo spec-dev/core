f52253ca20e3e737ec2497a4468004ebf6024bb0
Ben Whittle <benwhittle31@gmail.com>
2023-04-12 09:39:35 -0500

    add cors to core-api

--- core-api/src/routes/express.ts
+++ core-api/src/routes/express.ts
@@ -4,10 +4,12 @@ import config from '../config'
 import { specEnvs } from '../../../shared'
 import paths from '../utils/paths'
 import fileUpload from 'express-fileupload'
+import cors from 'cors'
 
 // Create Express app.
 export const app = express()
 app.use(fileUpload())
+app.use(cors())
 app.use(express.json({ limit: '50mb' }))
 if (config.ENV !== specEnvs.PROD) {
     app.use(morgan('dev'))
