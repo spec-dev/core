b748c1b64d5c4ba50ee511339baf4dd691ab4897
Ben Whittle <benwhittle31@gmail.com>
2023-05-15 04:24:29 -0500

    add EVENT_RELAY_PORT to k8s head reporter

--- head-reporters/tests/slightReorg.ts
+++ head-reporters/tests/slightReorg.ts
@@ -23,7 +23,7 @@ function newFakeBlockHeader(number: number): BlockHeader {
 } 
 
 async function fakeNewHeads(reporter: EvmReporter) {
-    let number = 17262919
+    let number = 17263944
     let lastReorgAt = number
     let hasFlipped = false
     setInterval(async() => {
