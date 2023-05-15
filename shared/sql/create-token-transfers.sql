bc34a6bd7101802323c5f8585a9e2429305c23ce
Ben Whittle <benwhittle31@gmail.com>
2023-05-15 01:03:26 -0500

    shared sql file adjustments

--- shared/sql/create-token-transfers.sql
+++ shared/sql/create-token-transfers.sql
@@ -3,15 +3,18 @@ create table if not exists tokens.token_transfers (
     id serial primary key,
     transfer_id character varying(70) not null,
     transaction_hash character varying(70),
+    log_index bigint,
     token_address character varying(50) not null,
     token_name character varying,
     token_symbol character varying,
     token_decimals bigint,
+    token_standard character varying,
+    token_id character varying,
     from_address character varying(50) not null,
     to_address character varying(50) not null,
-    is_mint boolean not null,
-    source character varying(20) not null,
-    value character varying not null,
+    is_mint boolean,
+    is_native boolean,
+    value character varying,
     value_usd numeric,
     value_eth numeric,
     value_matic numeric,
@@ -20,4 +23,12 @@ create table if not exists tokens.token_transfers (
     block_timestamp timestamp with time zone not null,
     chain_id character varying not null
 );
-create unique index "idx_er20_transfers_transfer_id" ON tokens.token_transfers(transfer_id);
+create unique index "idx_token_transfers_transfer_id" on tokens.token_transfers(transfer_id);
+create index "idx_token_transfers_by_block" on tokens.token_transfers(block_number, chain_id);
+create index "idx_token_transfers_by_timestamp" on tokens.token_transfers(block_timestamp);
+create index "idx_token_transfers_by_recipient" on tokens.token_transfers(to_address, chain_id);
+create index "idx_token_transfers_by_recipient_time" on tokens.token_transfers(to_address, chain_id, block_timestamp);
+create index "idx_token_transfers_by_sender" on tokens.token_transfers(from_address, chain_id);
+create index "idx_token_transfers_by_sender_time" on tokens.token_transfers(from_address, chain_id, block_timestamp);
+create index "idx_token_transfers_by_token" on tokens.token_transfers(token_address, chain_id);
+create index "idx_token_transfers_by_token_time" on tokens.token_transfers(token_address, chain_id, block_timestamp);
