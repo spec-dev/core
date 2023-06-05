63d295cb2d50f134b471047b545775a180df230a
mreedr <michael@waly.cash>
2023-06-05 14:13:43 -0400

    basic auth

--- core-api/src/routes/abi/abiRoutes.ts
+++ core-api/src/routes/abi/abiRoutes.ts
@@ -1,14 +1,15 @@
 import { app } from '../express'
 import paths from '../../utils/paths'
 import { parseGetAbiPayload } from './abiPayloads'
-import { codes, errors, authorizeAdminRequest } from '../../utils/requests'
+import { codes, errors, authorizeRequest } from '../../utils/requests'
 import { enqueueDelayedJob, getAbi, saveAbis } from '../../../../shared'
 
 /**
  * Get an ABI by chainId:address
  */
  app.get(paths.ABI, async (req, res) => {
-    if (!(await authorizeAdminRequest(req, res))) return
+    const user = await authorizeRequest(req, res)
+    if (!user) return
 
     // Parse & validate payload.
     const { payload, isValid, error } = parseGetAbiPayload(req.query)
