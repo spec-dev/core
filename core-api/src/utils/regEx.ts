b01ea366d5737da5f39b8e424ee66dbd670c0b96
Carly Doppelheuer <carlydoppelheuer@Carlys-MacBook-Pro-2.local>
2023-07-12 16:36:53 -0500

    update sql

---
+++ core-api/src/utils/regEx.ts
@@ -0,0 +1,6 @@
+export function regExSplitOnUppercase(column: string, isReplace: boolean) {
+    if (isReplace) {
+        column = `REPLACE(${column}, '.', ' ')`
+    }
+    return `REGEXP_REPLACE(REGEXP_REPLACE(REGEXP_REPLACE(${column}, '([0-9])([A-Z])', '\\1_\\2', 'g'), '([a-z])([A-Z])', '\\1_\\2', 'g'), '([A-Z])([A-Z][a-z])','\\1_\\2', 'g')`
+}
