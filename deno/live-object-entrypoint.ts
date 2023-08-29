02c4c33b3c805f5aebbec7c3b4377b750979e76b
Ben Whittle <benwhittle31@gmail.com>
2023-08-29 11:12:02 -0500

    updated live object entrypoint

--- deno/live-object-entrypoint.ts
+++ deno/live-object-entrypoint.ts
@@ -1,5 +1,5 @@
 import { serve } from 'https://deno.land/std@0.150.0/http/server.ts'
-import { Queue, StringKeyMap, BigInt } from 'https://esm.sh/@spec.dev/core@0.0.88'
+import { Queue, StringKeyMap, BigInt } from 'https://esm.sh/@spec.dev/core@0.0.106'
 import jwt from 'https://esm.sh/jsonwebtoken@8.5.1'
 import LiveObject from './spec.ts'
 import inputContractGroupAbis from './_abis.ts'
@@ -22,7 +22,6 @@ const headerNames = {
 }
 
 const config = {
-    // @ts-ignore
     JWT_SECRET: Deno.env.get('JWT_SECRET'),
     JWT_ROLE: 'internal',
 }
