d0c4f214b87e529be80555b1038c0774e497f781
Ben Whittle <benwhittle31@gmail.com>
2023-09-19 06:02:44 +0200

    remove code for tokens.transfers view

--- shared/sql/create-primitive-views.sql
+++ shared/sql/create-primitive-views.sql
@@ -81,33 +81,3 @@ SELECT
     block_timestamp,
     unnest(ARRAY['137'::text]) AS chain_id
 FROM polygon.transactions;
-
-CREATE OR REPLACE VIEW tokens.transfers AS
-SELECT
-    id,
-    transaction_hash,
-    token_address,
-    token_name,
-    token_symbol,
-    token_decimals,
-    from_address,
-    to_address,
-    is_mint,
-    value,
-    value_usd,
-    value_eth,
-    value_matic,
-    block_number,
-    block_hash,
-    block_timestamp
-    chain_id,
-    transfer_id,
-    log_index,
-    token_standard,
-    token_id,
-    CASE
-        WHEN is_native is true OR source = 'trace'
-            THEN true
-        ELSE false
-    END as is_native
-FROM tokens.token_transfers;
