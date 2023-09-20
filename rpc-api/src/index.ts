8a3c31e26ec73ffbcab8e1fb77d1d043e51078ef
Ben Whittle <benwhittle31@gmail.com>
2023-09-20 22:30:17 +0200

    add new rpc for resolving entire erc20 token metadata

--- rpc-api/src/index.ts
+++ rpc-api/src/index.ts
@@ -4,10 +4,11 @@ import config from './lib/config'
 import codes from './lib/codes'
 import paths from './lib/paths'
 import errors from './lib/errors'
-import { parseCallPayload, parseMetadataPayload } from './lib/payload'
+import { parseCallPayload, parseMetadataPayload, parseERC20TokenMetadataPayload } from './lib/payload'
 import { authRequest } from './lib/auth'
 import { specEnvs, logger, resolveMetadata } from '../../shared'
 import { callContract } from './services/callContract'
+import { getERC20TokenMetadata } from './services/getERC20TokenMetadata'
 
 // Create Express app.
 const app = express()
@@ -71,6 +72,32 @@ app.post(paths.METADATA, async (req, res) => {
     const resp = await resolveMetadata(protocolId, pointer)
     const code = resp.error?.code === codes.INTERNAL_SERVER_ERROR ? resp.error.code : codes.SUCCESS
     return res.status(code).json(resp)
+})
+
+/**
+ * Get ERC-20 token metadata.
+ */
+ app.post(paths.ERC20_TOKEN_METADATA, async (req, res) => {
+    // Auth the JWT to get RBAC role.
+    const claims = authRequest(req)
+    const role = claims?.role
+    if (!role) {
+        logger.error(errors.UNAUTHORIZED)
+        const [code, message] = [codes.UNAUTHORIZED, errors.UNAUTHORIZED]
+        return res.status(code).json({ error: { message, code } })
+    }
+
+    // Parse and validate payload.
+    const { payload, isValid, error } = parseERC20TokenMetadataPayload(req.body)
+    if (!isValid) {
+        const [code, message] = [codes.BAD_REQUEST, error || errors.INVALID_PAYLOAD]
+        return res.status(code).json({ error: { message, code } })
+    }
+    
+    // Proxy calls to/from smart contract.
+    const { chainId, tokenAddress } = payload
+    const resp = await getERC20TokenMetadata(chainId, tokenAddress)
+    return res.status(codes.SUCCESS).json(resp)
 })
 
 ;(async () => {
